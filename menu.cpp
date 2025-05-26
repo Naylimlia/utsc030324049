71804c8cc580c176014f938db34704ce5eb5a79b
Naylimlia <nayliamalia42@gmail.com>
2025-05-26 12:54:04 +0800

    Menyederhanakan penulisan output array dengan ternary operator

--- menu.cpp
+++ menu.cpp
@@ -45,24 +45,19 @@ void outputData() {
 }
 
 void bubbleSorting() {
-  system("cls");
-  
   for (int i = 0; i < n - 1; ++i) {
       for (int j = 0; j < n - i - 1; ++j) {
           if (arr[j] > arr[j + 1]) {
-              int temp = arr[j];
-              arr[j] = arr[j + 1];
-              arr[j + 1] = temp;
+            swap(arr[j], arr[j + 1]);   
           }
       }
   }
 
   cout << "Array setelah diurutkan: ";
   for (int i = 0; i < n; ++i) {
-      cout << arr[i];
-      if (i != n - 1) cout << ", "; 
+      cout << arr[i] << (i != n - 1 ? ", " : "");
   }
-  getch();
+  cin.get();
 }
 
 void sepatahKata() {
